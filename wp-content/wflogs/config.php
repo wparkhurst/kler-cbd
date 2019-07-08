7ebabd181d885b56060b11a657d266409b18b6be
wayneparkhurst <wayne@kler.io>
2019-07-08 15:10:39 -0700

    Fix media

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1563219709;s:7:"authKey";s:64:"*}Y4AggQC&VT$_H4j3h54b4Lt|7)6mI&~0~ r,Vw3R,:ZVbVGo]T)cWaL9u<8B?5";s:7:"version";s:5:"1.0.4";}
